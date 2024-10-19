60f2181b45d70ec77cfa7fd8265a25404f81413f
Vikash Thakur <thakvikash622@gmail.com>
2024-10-19 23:42:01 +0530

    Fixed: motion in Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,21 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useMotionValueEvent, useScroll, useTransform } from 'framer-motion'
 
 const Hero = () => {
+
+    const heroRef = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: heroRef,
+        offset: ['start end', 'end start']
+    })
+
+
+    useMotionValueEvent(scrollYProgress, 'change', (latest) => console.log(latest))
+    const translateY = useTransform(scrollYProgress, [0,1], [150, -150])
     return (
-        <section className='p-5 mt-2 md:mt-5 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
+        <section ref={heroRef} className='p-5 mt-2 md:mt-5 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
             <div className='md:grid md:grid-cols-2 items-center'>
                 <div className='md:max-w-4xl md:px-10 md:py-10'>
                     <span className='border border-[#222]/10 inline p-2 rounded-md'>Version 2.0 is here</span>
@@ -24,26 +36,43 @@ const Hero = () => {
                     </div>
                 </div>
                 <div className='mt-20 flex items-center justify-center relative'>
-                    <Image
+                    <motion.img
                         src={"/assets/cog.png"}
                         alt='Arrow Right'
                         width={500}
                         height={100}
                         className=''
+                        animate={{
+                            translateY: [-30, 30],
+                        }}
+                        transition={{
+                            repeat: Infinity,
+                            repeatType: "mirror",
+                            duration: 3,
+                            ease: "easeInOut"
+
+                        }}
                     />
-                    <Image
+                    <motion.img
                         src={"/assets/cylinder.png"}
                         alt='Arrow Right'
                         width={200}
                         height={100}
                         className='hidden md:block absolute md:-top-20 md:-left-24 lg:-top-20 lg:-left-0'
+                        style={{
+                            translateY: translateY,
+                        }}
                     />
-                    <Image
+                    <motion.img
                         src={"/assets/noodle.png"}
                         alt='Arrow Right'
                         width={200}
                         height={100}
                         className='hidden lg:block absolute -bottom-20 right-0 rotate-[40deg]'
+                        style={{
+                            rotate: 30,
+                            translateY: translateY, 
+                        }}
                     />
                 </div>
             </div>
